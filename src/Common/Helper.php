9de33009fdd2fe65e1a1cef5b9096e081028005a
adrian <a.zapotoczny@savicki.pl>
2020-04-15 09:18:35 +0200

    helper

--- src/Common/Helper.php
+++ src/Common/Helper.php
@@ -4,22 +4,22 @@ namespace Sylapi\Courier\Common;
 
 class Helper
 {
-    static function guid()
-    {
+    static function guid() {
+
         mt_srand((double)microtime()*10000);
         $charid = strtoupper(md5(uniqid(rand(), true)));
         $retval = substr($charid, 0, 32);
         return $retval;
     }
 
-    static function toArray($data)
-    {
+    static function toArray($data) {
+
         $json = json_encode($data);
         return json_decode($json, true);
     }
 
-    static function toAmonut($price)
-    {
+    static function toAmonut($price) {
+
         return str_replace(array(',', ' '), array('.', ''), $price);
     }
 }
