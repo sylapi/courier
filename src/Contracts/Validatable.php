078bbaa44f0fcd44c1f4495d88811d940e47935c
Daniel Serafinski <d.serafinski@sylapi.com>
2021-02-23 10:24:23 +0100

    Fix: Validatable

--- src/Contracts/Validatable.php
+++ src/Contracts/Validatable.php
@@ -7,6 +7,6 @@ namespace Sylapi\Courier\Contracts;
 interface Validatable
 {
     public function getErrors(): array;
-
+    public function setErrors(array $errors): self;
     public function validate(): bool;
 }
