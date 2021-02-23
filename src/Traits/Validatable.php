fd133e9bceff6b20ef06004fbd88d0fed72c99c8
Daniel Serafinski <d.serafinski@sylapi.com>
2021-02-23 10:20:02 +0100

    Fix: Validatable

--- src/Traits/Validatable.php
+++ src/Traits/Validatable.php
@@ -8,6 +8,12 @@ trait Validatable
 {
     private $errors;
 
+    public function setErrors(array $errors): self
+    {
+        $this->errors = $errors;
+        return $this;
+    }
+
     public function getErrors(): array
     {
         return $this->errors ?? [];
