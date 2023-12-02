29320f31c8d348da898b106df9d74cdec600a8b2
Daniel Serafiński <d.serafinski@savicki.pl>
2023-12-02 13:00:14 +0100

    WIP

--- src/Abstracts/Options.php
+++ src/Abstracts/Options.php
@@ -8,4 +8,35 @@ use Sylapi\Courier\Traits\Validatable;
 abstract class Options implements OptionsContract
 {
     use Validatable;
+
+    private $options;
+
+    public function __construct(array $options = [])
+    {
+        $this->options = $options;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->options);
+    }
+
+    public function get(string $key)
+    {
+        if ($this->has($key)) {
+            return $this->options[$key];
+        }
+
+        return null;
+    }
+
+    public function set(string $key, $value): void
+    {
+        $this->options[$key] = $value;
+    }
+
+    public function all(): array
+    {
+        return $this->options;
+    }    
 }
