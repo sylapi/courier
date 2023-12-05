75148f614fe66883b19aeaa7894952757d8ffa9c
Daniel Serafiński <d.serafinski@savicki.pl>
2023-12-05 14:21:14 +0100

    WIP

--- src/Abstracts/Services/PickupPoint.php
+++ src/Abstracts/Services/PickupPoint.php
@@ -4,18 +4,18 @@ namespace Sylapi\Courier\Abstracts\Services;
 
 use Sylapi\Courier\Abstracts\Service;
 use Sylapi\Courier\Traits\Validatable;
-use Sylapi\Courier\Contracts\Services\COD as CODContract;
+use Sylapi\Courier\Contracts\Services\PickupPoint as PickupPointContract;
 
-abstract class PickupPoint extends Service implements CODContract
+abstract class PickupPoint extends Service implements PickupPointContract
 {
     use Validatable;
 
-    public function getPickupId(): ?float
+    public function getPickupId(): ?string
     {
         return $this->get('pickupId', null);
     }
 
-    public function setPickupId(string $pickupId): CODContract
+    public function setPickupId(string $pickupId): PickupPointContract
     {
         $this->set('pickupId', $pickupId);
         return $this;
