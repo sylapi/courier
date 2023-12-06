88bf395fa33c2eec0cb9422c260e2114ec5a503a
Daniel Serafiński <d.serafinski@savicki.pl>
2023-12-06 09:04:52 +0100

    WIP

---
+++ src/Response/Parcel.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylapi\Courier\Response;
+
+use Sylapi\Courier\Abstracts\Response as ResponseAbstract;
+use Sylapi\Courier\Contracts\Response as ResponseContract;
+
+class Parcel extends ResponseAbstract implements ResponseContract
+{
+    private $shipmentId;
+
+    public function setShipmentId(string $ShipmentId): ResponseContract
+    {
+        $this->shipmentId = $ShipmentId;
+
+        return $this;
+    }
+
+    public function getShipmentId(): ?string
+    {
+        return $this->shipmentId;
+    }
+}
