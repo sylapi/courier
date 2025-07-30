1466bb104315ce58d43c378e74ae3486b30b8f36
Daniel Serafiński <d.serafinski@savicki.pl>
2025-07-30 12:49:05 +0200

    php 8.0

--- src/Enums/StatusType.php
+++ src/Enums/StatusType.php
@@ -5,26 +5,27 @@ declare(strict_types=1);
 namespace Sylapi\Courier\Enums;
 
 
-enum StatusType: string {
-    case APP_UNAVAILABLE = 'app_unavailable';
-    case APP_RESPONSE_ERROR = 'app_response_error';
-    case NEW = 'new';
-    case CANCELLED = 'cancelled';
-    case DELIVERED = 'delivered';
-    case ENTRY_WAIT = 'entry_wait';
-    case ORDERED = 'ordered';
-    case SENT = 'sent';
-    case SPEDITION_DELIVERY = 'spedition_delivery';
-    case PICKUP_READY = 'pickup_ready';
-    case LOST = 'lost';
-    case REFUND = 'refund';
-    case WAREHOUSE_ENTRY = 'warehouse_entry';
-    case RETURN_DELIVERY = 'return_delivery';
-    case SOLVING = 'solving';
-    case RETURNING = 'returning_back';
-    case RETURNED = 'returned';
-    case PROCESSING = 'processing';
-    case PROCESSING_FAILED = 'processing_failed';
-    case DELETED = 'deleted';
+class StatusType {
+    public const APP_UNAVAILABLE = 'app_unavailable';
+    public const APP_RESPONSE_ERROR = 'app_response_error';
+    public const NEW = 'new';
+    public const CANCELLED = 'cancelled';
+    public const DELIVERED = 'delivered';
+    public const ENTRY_WAIT = 'entry_wait';
+    public const ORDERED = 'ordered';
+    public const SENT = 'sent';
+    public const SPEDITION_DELIVERY = 'spedition_delivery';
+    public const PICKUP_READY = 'pickup_ready';
+    public const LOST = 'lost';
+    public const REFUND = 'refund';
+    public const WAREHOUSE_ENTRY = 'warehouse_entry';
+    public const RETURN_DELIVERY = 'return_delivery';
+    public const SOLVING = 'solving';
+    public const RETURNING = 'returning_back';
+    public const RETURNED = 'returned';
+    public const PROCESSING = 'processing';
+    public const PROCESSING_FAILED = 'processing_failed';
+    public const DELETED = 'deleted';
+
 }
 
